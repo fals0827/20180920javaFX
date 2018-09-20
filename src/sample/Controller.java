b4ac294e3ed9fd34fcd62c373e1701d68f370e9f
賴品宇 <32055747+fals0827@users.noreply.github.com>
2018-09-20 12:36:22 +0800

    20180920

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void pushCount () {
+        System.out.println("hdfjksadhfkja");
+    }
+}
